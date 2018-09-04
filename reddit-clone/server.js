4ae8f02243c8e33640359eb30c9c7ec704217730
Johnathan Chen <johnathanchenn@outlook.com>
2018-09-04 09:56:43 -0700

    push

---
+++ reddit-clone/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.get('/posts/new', function (req, res) {
+  res.render('posts-new')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
